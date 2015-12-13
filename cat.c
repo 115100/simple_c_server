d6d2a9b507aca16f9ad2dc9dd245e11f048bdd84
115100 <b>
2015-12-13 17:21:46 +0000

    Write file contents to body

--- cat.c
+++ cat.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "cat.h"
+#include "string.h"
 
 
-int cat(char *file)
+int cat(char *file, char *body)
 {
     FILE *fp;
 
@@ -13,15 +15,32 @@ int cat(char *file)
         return 1;
     }
 
-    else
+    int c;
+    long int fileLength;
+
+    fseek(fp, 0, SEEK_END);
+
+    fileLength = ftell(fp);
+
+    fseek(fp, 0, SEEK_SET);
+
+    char *originalPointer = body;
+
+    // Null byte?
+    body = malloc(fileLength + 2);
+
+    // There's probably a better way to do this
+    while ((c = getc(fp)) != EOF)
     {
-        int c;
-        
-        while ((c = getc(fp)) != EOF)
-        {
-            putc(c, stdout); // TODO: Write to response.body
-        }
+        *originalPointer = (char) c;
+        originalPointer++;
     }
 
+    // Terminating byte
+    *originalPointer = '\0';
+
+    close(fp);
+
+    // Don't forget to free *body
     return 0;
 }
--- include/cat.h
+++ include/cat.h
@@ -1,6 +1,6 @@
 #ifndef CAT_H
 #define CAT_H
 
-int cat(char *file); // We only need to cat one file at a time
+int cat(char *file, char *body);
 
 #endif // CAT_H
