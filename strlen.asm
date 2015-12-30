0374a81c3b4825ebbed18cac5cad36227711a576
115100 <115100@users.noreply.github.com>
2015-12-30 17:41:29 +0000

    fix comment

--- strlen.asm
+++ strlen.asm
@@ -8,7 +8,7 @@ section .text
 strlen:
 	xor rax, rax ; clear retval register
 	mov rcx, -1 ; set rcx to ...111
-	repne scasb ; compare [rdi] to AL (...000), dec rax each loop and inc rdi
+	repne scasb ; compare [rdi] to AL (...000), dec rcx each loop and inc rdi
 	not rcx ; reverse bits
 	dec rcx ; remove count of null byte
 	mov rax, rcx ; put length into retval
