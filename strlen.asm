e268a32d9ed6a2ec29837e8cf419c00831cdfaf4
115100 <115100@users.noreply.github.com>
2015-12-30 17:38:46 +0000

    remove a few instructions

--- strlen.asm
+++ strlen.asm
@@ -6,13 +6,10 @@ section .text
 	global strlen
 
 strlen:
-	push rbp
-	mov rbp, rsp
 	xor rax, rax ; clear retval register
 	mov rcx, -1 ; set rcx to ...111
 	repne scasb ; compare [rdi] to AL (...000), dec rax each loop and inc rdi
 	not rcx ; reverse bits
 	dec rcx ; remove count of null byte
 	mov rax, rcx ; put length into retval
-	pop rbp ; pop base pointer
 	ret
